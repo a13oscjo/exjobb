3a195cfcfc480bfbf099ddfc9be0833c5444082f
a13oscjo <a13oscjo@student.his.se>
2017-05-30 14:29:36 +0200

    testing..

--- AutoMsi.ps1
+++ AutoMsi.ps1
@@ -1,16 +1,16 @@
-param(
- [Parameter(Mandatory=$True)]
- [string]
- $username,
+#param(
+# [Parameter(Mandatory=$True)]
+# [string]
+# $username,
 
- [Parameter(Mandatory=$True)]
- [string]
- $url,
+# [Parameter(Mandatory=$True)]
+# [string]
+# $url,
 
- [Parameter(Mandatory=$True)]
- [string]
- $filename
-)
+# [Parameter(Mandatory=$True)]
+# [string]
+# $filename
+#)
 ###########################################################################################
 # Downloads and installs msi packages silently.                                           #
 #Parameters are used for automated installation, uncomment variables for manual usage     #
@@ -18,23 +18,23 @@ param(
 
 #VARIABLES FOR MANUAL EXECUTION!
 #"URL to the resource"
-#$url ="http://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B03FE9563-80F9-119F-DA3D-72FBBB94BC26%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi?brand=GCEA"
+$url ="http://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B03FE9563-80F9-119F-DA3D-72FBBB94BC26%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi?brand=GCEA"
 #"The name of the resource with file extension e.g. script.ps1, program.exe"
-#$filename = "googlechromestandaloneenterprise64.msi"
+$filename = "googlechromestandaloneenterprise64.msi"
 # "Username of the account used during installation"
 #$username = Read-Host
 
-$PathtoFile = "$env:HOMEDRIVE\Users\$username\Resources"
+$PathtoFile = "$env:HOMEDRIVE\Resources"
 
-if (-not (Test-Path "$env:HOMEDRIVE\Users\$username" -PathType Container))
+if (-not (Test-Path "$env:HOMEDRIVE\Resources" -PathType Container))
 {
-    New-Item "$env:HOMEDRIVE\Users\$username" -itemType directory
+    New-Item "$env:HOMEDRIVE\Resources" -itemType directory
 }
 
-if (-not (Test-Path "$env:HOMEDRIVE\Users\$username\Resources" -PathType Container))
-{
-    New-Item "$env:HOMEDRIVE\Users\$username\Resources" -itemType directory
-}
+#if (-not (Test-Path "$env:HOMEDRIVE\Users\$username\Resources" -PathType Container))
+#{
+#    New-Item "$env:HOMEDRIVE\Users\$username\Resources" -itemType directory
+#}
 
 $output = "$PathtoFile\$filename"
 
