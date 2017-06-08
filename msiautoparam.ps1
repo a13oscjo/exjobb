16a823c6db5d0b06e0a195aa20762b53220530d0
a13oscjo <a13oscjo@student.his.se>
2017-06-08 16:18:39 +0200

    Update msiautoparam.ps1

--- msiautoparam.ps1
+++ msiautoparam.ps1
@@ -5,11 +5,11 @@ param(
 
  [Parameter(Mandatory=$True)]
  [string]
- $url,
+ $url
 
- [Parameter(Mandatory=$True)]
- [string]
- $filename
+# [Parameter(Mandatory=$True)]
+# [string]
+# $filename
 )
 ###########################################################################################
 # Downloads and installs msi packages silently.                                           #
@@ -20,12 +20,11 @@ param(
 #"URL to the resource"
 #$url ="http://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B03FE9563-80F9-119F-DA3D-72FBBB94BC26%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable/dl/chrome/install/googlechromestandaloneenterprise64.msi?brand=GCEA"
 #"The name of the resource with file extension e.g. script.ps1, program.exe"
-#$filename = "googlechromestandaloneenterprise64.msi"
 #"Username of the account used during installation"
 #$username = Read-Host
 
 $PathtoFile = "C:\Resources"
-
+$number = 1
 if (-not (Test-Path "C:\Resources" -PathType Container))
 {
     New-Item "C:\Resources" -itemType directory
@@ -36,8 +35,15 @@ if (-not (Test-Path "C:\Resources" -PathType Container))
 #    New-Item "C:\Users\$username\Resources" -itemType directory
 #}
 
+$filename = "msiinstaller$number.msi"
 $output = "$PathtoFile\$filename"
-
+$test = Test-Path "$output"
+while ($test -eq $true){
+    $number = $number+1
+    $filename = "msiinstaller$number.msi"
+    $output = "$PathtoFile\$filename"
+    $test = Test-Path "$output"
+}
 Invoke-WebRequest -Uri $url -Outfile $output
 
 msiexec /i $output /quiet /qn /norestart
